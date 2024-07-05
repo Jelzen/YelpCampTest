f2a6f894b53a39db5482f3fa60ce3ab0c2035872
Jelzen <jvonsa@gmail.com>
2024-07-05 15:31:21 -0600

    added storeReturnTo to middleware.

--- middleware.js
+++ middleware.js
@@ -3,6 +3,13 @@ const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
 module.exports.LocalStrategy = (req, res, next) => {
     if (req.session.returnTo) {res.local.returnTo = req.session.returnTo;        
     }
